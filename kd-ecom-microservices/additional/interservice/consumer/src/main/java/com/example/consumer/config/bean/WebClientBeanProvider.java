51ae217769b08ecdb1d6743d6625545aad806630
Kalidas Khupte <251994276+kalidas-khupte-fullStatckDev@users.noreply.github.com>
2026-02-22 20:21:14 +0530

    Worked on Http Interface Service using RestTemplate,RestClient & WebClient via Service Discovery

--- kd-ecom-microservices/additional/interservice/consumer/src/main/java/com/example/consumer/config/bean/WebClientBeanProvider.java
+++ kd-ecom-microservices/additional/interservice/consumer/src/main/java/com/example/consumer/config/bean/WebClientBeanProvider.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
--- kd-ecom-microservices/additional/interservice/consumer/src/main/java/com/example/consumer/service/communication/openfeign/service/OpenFeignProvider.java
+++ kd-ecom-microservices/additional/interservice/consumer/src/main/java/com/example/consumer/service/communication/openfeign/service/OpenFeignProvider.java
@@ -2,7 +2,6 @@ package com.example.consumer.service.communication.openfeign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 //@FeignClient( name = "provider-service", url= "http://localhost:8081/api")
 @FeignClient(name = "provider-service/api")
